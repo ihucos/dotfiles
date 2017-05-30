626b42a33fa47db81279d0ba93e98af9e14bab03
Irae Hueck Costa <irae.hueck.costa@gmail.com>
2017-05-30 15:29:40 +0200

    Update

--- bash/.bashrc.d/tmux.bash
+++ bash/.bashrc.d/tmux.bash
@@ -1,4 +1,4 @@
-# if [ -n "$TMUX" ] ; then
-#   tmux set status-right-length 120
-#   PROMPT_COMMAND='((tmux set -g status-right "#(cd #{pane_current_path} && git rev-parse --abbrev-ref HEAD) | #(cd #{pane_current_path} && git status --porcelain 2> /dev/null | grep -c ^) | $? ") & ); '"$PROMPT_COMMAND"
-# fi
+if [ -n "$TMUX" ] ; then
+  tmux set status-right-length 120
+  PROMPT_COMMAND='((tmux set -g status-right "#(cd #{pane_current_path} && git rev-parse --abbrev-ref HEAD) | #(cd #{pane_current_path} && git status --porcelain 2> /dev/null | grep -c ^) | $? ") & ); '"$PROMPT_COMMAND"
+fi
