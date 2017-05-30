3d9be1476b4ffdacafecfb9c6374cead712eba91
Irae Hueck Costa <irae.hueck.costa@gmail.com>
2017-05-30 15:27:43 +0200

    Update

--- bash/.bashrc.d/prompt.bash
+++ bash/.bashrc.d/prompt.bash
@@ -1,1 +1,36 @@
-PS1='\w '"\[\033[38;5;15m\]$\[\033[0m\] "
+PS1='$(_prompt_exit_status) \w '"\[\033[38;5;15m\]$\[\033[0m\] \$(_prompt_git)"
+
+_prompt_exit_status(){
+
+	EXIT=$?
+	if [ $EXIT != 0 ]; then
+		tput setaf 1
+		printf "$EXIT"
+		tput sgr0
+	fi
+}
+
+_prompt_git(){
+	# name=$(git symbolic-ref --quiet HEAD || true)
+	# echo $name
+	:
+
+}
+# prompt(){
+# 	[[ $(git rev-parse --is-inside-work-tree) == true ]] ||
+# 		return
+# 	name=$(
+# 		git symbolic-ref --quiet HEAD ||
+# 		git describe --tags --exact-match HEAD ||
+# 		git rev-parse --short HEAD
+# 	) || return
+# 	name=${name#refs/*/}
+# 	[[ -n $name ]] || return
+# 	printf "\033[1K\r"
+# 	dir=$(git rev-parse --show-prefix)
+# 	printf "$name $dir $ "
+# }
+
+# PROMPT_COMMAND="prompt; $PROMPT_COMMAND"
+# PROMPT_COMMAND=prompt
+
